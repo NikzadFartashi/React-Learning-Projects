bd3d127790c3e525f41b318e43edfc0a72106c22
NikzadFartashi <nikzad.fartashi83@gmail.com>
2025-08-22 20:39:48 +0330

    first component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString(),
+    });
+  }
+
+  render() {
+    return <h2>It is {this.state.time}</h2>;
+  }
+}
+
+class Hello extends React.Component {
+  render() {
+    return <h1>سلام دوستان من</h1>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Timer />
+      </div>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const tick = () => {
-  const elem = (
-    <div>
-      <h1>سلام دوستان من</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  root.render(elem);
-};
-
-// هر ثانیه یکبار tick اجرا میشه
-setInterval(tick, 1000);
+root.render(<App />);
